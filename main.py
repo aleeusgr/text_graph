029524defe31fa3e79e5991fd68e550305a87560
Alex Seregin <alexeusgr@yahoo.com>
2021-02-02 12:35:25 +0300

    color fix, export to gephi

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 from text import txt_pipe
 import networkx as nx
 
-source = 'dream.txt'
+#source = 'dream.txt'
 source = 'short.txt'
 
 nodes, grams = txt_pipe(source)
 # mistake!(?? still works) not using BAG.
 
 def graph():
-    # create and vis the graph:
+    # create and the graph:
     G = nx.Graph()
     G.add_nodes_from(nodes)
     # edges from ngrams:
@@ -20,24 +20,25 @@ def graph():
     return G
 
 G = graph()
+#nx.write_gexf(G, "{}.gexf".format(source)) 
 
 def vis():
-    
+    '''Visualise'''
     # for edge thickness:
+
     edges = G.edges
     weights = [G[u][v]['weight'] for u,v in edges]
 
     import matplotlib.pyplot as plt
-
-#vis()
+    
+    # Color nodes by POS tag
     import matplotlib.colors as mcolors
-    color_list = mcolors.cnames.keys()
+    color_list = mcolors.XKCD_COLORS
     tags = [u[1] for u in G.nodes(data="POS")]
     color_map = dict(zip(set(tags),color_list))
     colors =  [color_map[u[1]] for u in G.nodes(data='POS')]
 
     nx.draw(G, with_labels=True,width=weights,node_color = colors)
     plt.show()
-# convert node POS tag into collection, assign color 
 
-vis()
+#vis()
